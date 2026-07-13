cb1fce34561c5e9ca294d32538045c0892dffa6f
Star Mdakane <mmdakane@rocketmail.com>
2026-07-13 20:40:09 +0200

    initial commit

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Rubik } from 'next/font/google'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const rubik = Rubik({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={rubik.className}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
