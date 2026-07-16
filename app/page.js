b3235885b8b27c9a7db15dee00f5b07a818fcbab
Star Mdakane <mmdakane@rocketmail.com>
2026-07-16 19:46:06 +0200

    replies ui w/o interactivity

--- app/page.js
+++ app/page.js
@@ -6,12 +6,16 @@ export default function Home() {
   const currentUser = post.currentUser;
   const userName = currentUser.username;
   const comments = post.comments;
-  console.log(comments);
+  // console.log(comments);
+
+  const replies = () => {
+
+  }
 
   return (
 
 
-    <div className=" w-86 md:w-171 lg:w-182 mx-auto flex flex-col gap-4 md:gap-6">
+    <div className="w-86 md:w-171 lg:w-182 mx-auto flex flex-col">
       {comments.map(comment => (
         <Comment key={comment.id} comment={comment} />
       ))}
--- components/Comment.jsx
+++ components/Comment.jsx
@@ -12,7 +12,7 @@ const Comment = ({ comment }) => {
 
     return (
         <>
-            <div className='bg-white rounded-lg p-4 grid gap-4 md:gap-x-6 grid-cols-2 md:grid-cols-[40px_1fr_1fr]'>
+            <div className='bg-white rounded-lg p-4 grid gap-4 md:gap-x-6 grid-cols-2 md:grid-cols-[40px_1fr_1fr] mb-4 md:mb-6'>
                 <div className='w-full flex col-span-full md:row-start-1 md:col-start-2 md:col-span-1 gap-4 items-center'>
                     <Image
                         src={image}
@@ -22,7 +22,7 @@ const Comment = ({ comment }) => {
                     <p className='text-[16px] text-text-bold leading-[150%] tracking-normal font-medium'>{username}</p>
                     <p className='text-[16px] leading-[150%] tracking-normal font-normal'>{comment.createdAt}</p>
                 </div>
-                <p className='col-span-full md:col-start-2 md:col-span-2'>
+                <p className='col-span-full md:col-start-2 md:col-span-2 text-[16px] leading-[150%] tracking-normal font-normal'>
                     {comment.content}
                 </p>
                 <div className='flex items-center justify-around px-2 md:py-2 bg-bg rounded-[10px] h-10 w-25 md:w-10 md:h-25 md:flex-col md:col-start-1 md:col-end-2 md:row-start-1 md:row-end-3'>
@@ -34,14 +34,18 @@ const Comment = ({ comment }) => {
                         <FaMinus className='text-[10px] text-[#C5C6EF]' />
                     </button>
                 </div>
-                <div className='flex justify-self-end md:row-start-1  text-pri items-center gap-2'>
+                <button className='flex justify-self-end md:row-start-1  text-pri items-center gap-2 cursor-pointer'>
                     <FaReply className='text-[14px]' />
                     <span className='text-[16px] leading-[150%] tracking-normal font-medium'>Reply</span>
-                </div>
+                </button>
             </div>
             {replies.map(reply => (
-                <Reply key={reply.id} reply={reply} />
+                <div key={reply.id} >
+
+                    <Reply reply={reply} />
+                </div>
             ))}
+
         </>
 
     )
